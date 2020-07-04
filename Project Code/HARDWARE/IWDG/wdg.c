ba7bde5eea0e9abecd26a388ff00b02741d2b9b7
Lsorange <52439855+Lsorange@users.noreply.github.com>
2020-07-04 17:07:17 +0800

    Update wdg.c

--- Project Code/HARDWARE/IWDG/wdg.c
+++ Project Code/HARDWARE/IWDG/wdg.c
@@ -6,58 +6,58 @@ static void IWDG_Init(u8 prer,u16 rlr);
 volatile u8 WDG_Flg = 1;
 
 /*****************************************************************************
-�����ܣ����Ź�ģ��Ļص�����
-����汾��V1.0
-��ڲ�������
-���ز�������
-��    �ڣ�2019/6/18
-��    �ߣ�Orange
-��    �ģ���
+程序功能：看门狗模块的回调函数
+程序版本：V1.0
+入口参数：无
+返回参数：无
+日    期：2019/6/18
+作    者：Orange
+修    改：无
 ******************************************************************************/
 
 void MWDG_CallBack(void)
 {
-	IWDG_ReloadCounter();    //���¸����ſڼ��س�ֵ�������������ʱ�䳬��1s����Ƭ���ͻḴλ
+	IWDG_ReloadCounter();    //重新给看门口加载初值，如果程序运行时间超过10s，单片机就会复位
 }
 
 
 /*****************************************************************************
-�����ܣ����Ź�ģ��ĳ�ʼ��
-����汾��V1.0
-��ڲ�������
-���ز�������
-��    �ڣ�2019/6/18
-��    �ߣ�Orange
-��    �ģ���
+程序功能：看门狗模块的初始化
+程序版本：V1.0
+入口参数：无
+返回参数：无
+日    期：2019/6/18
+作    者：Orange
+修    改：无
 ******************************************************************************/
 
 void MWDG_Init(void)
 {
 	
-	IWDG_Init(PRER, RLR);  //��������ʱ�䳬��10s����Ƭ���ͻḴλ
+	IWDG_Init(PRER, RLR);  //程序运行时间超过10s，单片机就会复位
 }
 
 /*****************************************************************************
-�����ܣ����Ź��ĳ�ʼ������
-����汾��V1.0
-��ڲ�����*Data_BUF
-���ز�������
-��    �ڣ�2019/6/18
-��    �ߣ�Orange
-��    �ģ���
+程序功能：看门狗的初始化配置
+程序版本：V1.0
+入口参数：*Data_BUF
+返回参数：无
+日    期：2019/6/18
+作    者：Orange
+修    改：无
 ******************************************************************************/
 
 static void IWDG_Init(u8 prer,u16 rlr) 
 {	
- 	IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);  //ʹ�ܶԼĴ���IWDG_PR��IWDG_RLR��д����
+ 	IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);  //使能对寄存器IWDG_PR和IWDG_RLR的写操作
 	
-	IWDG_SetPrescaler(prer);  //����IWDGԤ��Ƶֵ:����IWDGԤ��ƵֵΪ64
+	IWDG_SetPrescaler(prer);  //设置IWDG预分频值:设置IWDG预分频值为64
 	
-	IWDG_SetReload(rlr);  //����IWDG��װ��ֵ,Tout=((4��2^prer) ��rlr) /40 (ms)
+	IWDG_SetReload(rlr);  //设置IWDG重装载值,Tout=((4×2^prer) ×rlr) /40 (ms)
 	
-	IWDG_ReloadCounter();  //����IWDG��װ�ؼĴ�����ֵ��װ��IWDG������
+	IWDG_ReloadCounter();  //按照IWDG重装载寄存器的值重装载IWDG计数器
 	
-	IWDG_Enable();  //ʹ��IWDG
+	IWDG_Enable();  //使能IWDG
 }
 
 
